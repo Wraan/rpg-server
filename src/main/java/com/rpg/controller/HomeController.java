24279278cb6b3fba9246b967bf33508350ad5d26
Kamil Drewniak <zibix12@gmail.com>
2019-10-06 13:25:41 +0200

    Swagger added and configured

--- src/main/java/com/rpg/controller/HomeController.java
+++ src/main/java/com/rpg/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.rpg.controller;
 
 import com.rpg.model.Character;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +18,10 @@ public class HomeController {
     }
 
     @GetMapping("/api/v1/test")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "Bearer access_token", required = true, dataType = "String",
+                    paramType = "header", defaultValue="Bearer access-token")
+    })
     public Character test(){
         return new Character("Marcin", "Mouse", "Thief");
     }
